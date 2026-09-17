c04b0c6e737ae4a9205d3b6e918b3c6786fc8f09
Suzana Zatorre <suzanazatore@suzanazatorre.com.br>
2026-09-17 10:08:27 -0300

    feat: renovar ícone do App Rotina

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -17,13 +17,13 @@ export default function manifest() {
         src: '/pwa-icon-192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any',
+        purpose: 'any maskable',
       },
       {
         src: '/pwa-icon-512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any',
+        purpose: 'any maskable',
       },
     ],
   }
